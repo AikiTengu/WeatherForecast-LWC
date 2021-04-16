ade298a8bc2dca9654bba5b6a0b68513e91e3933
AikiTeng <aiki_tengu@yahoo.com>
2021-04-16 11:17:56 +0300

    Parsing into DTO

--- force-app/main/default/lwc/weatherForecast/weatherForecast.js
+++ force-app/main/default/lwc/weatherForecast/weatherForecast.js
@@ -1,43 +1,43 @@
 import { LightningElement, api, track, wire } from "lwc";
 import getCity from "@salesforce/apex/AccountLocationManager.getCity";
-import getWeatherByCity from "@salesforce/apex/AccountWeatherConsumer.getWeatherByCity"; 
-export default class WeatherForecast extends LightningElement {	
-    //getting accountId
-    @api recordId;
+import getWeatherByCity from "@salesforce/apex/AccountWeatherConsumer.getWeatherByCity";
+export default class WeatherForecast extends LightningElement {
+	//getting accountId
+	@api recordId;
 
-	city;	
-    weather;
-    temp;
-    humidity;
-    pressure;
-    wind;
-    clouds;
+	city;
+	weather;
+	temp;
+	humidity;
+	pressure;
+	wind;
+	clouds;
 
+	@wire(getCity, { accountId: "$recordId" })
+	wireCity({ error, data }) {
+		if (data) {
+			this.city = data;
+		}
+		if (error) {
+			this.error = error;
+		}
+	}
 
-    @wire(getCity, { accountId: '$recordId'})
-    wireCity({error, data}){
-        if (data) {
-            this.city = data;
-        }
-        if (error) {
-            this.error = error;
-        }
-    }
+	@wire(getWeatherByCity, { city: "$city" })
+	wireWeatherDTO({ error, data }) {
+		if (data) {
+			console.log(data);
+			this.weather = data;
+			this.temp = this.weather.main.temp;
+			this.humidity = this.weather.main.humidity;
+			this.pressure = this.weather.main.pressure;
+		}
+		if (error) {
+			this.error = error;
+		}
+	}
 
-    @wire(getWeatherByCity, {city: '$city'})
-    wireWeatherDTO({error, data}) {
-        if (data) {
-            this.weather = data;
-            this.temp=this.weather.temperature;
-            this.humidity=this.weather.humidity;
-            this.pressure=this.weather.pressure;
-        }
-        if (error) {
-            this.error = error;
-        }
-    }
-
-    get errors() {
+	get errors() {
 		return this.error ? reduceErrors(this.error) : [];
 	}
 }
